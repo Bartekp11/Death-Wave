37b1ddbb3b8148d16e2c1fe11dfe00e23833f83a
Bartek Pawlik <pawlik711@gmail.com>
2020-12-21 18:19:58 +0100

    Settings menu
    Zmiana rozdzielczości, trybu pełnego ekranu i ustawienia głośności

---
+++ Assets/Audio/VolumeSlider.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    private AudioSource AudioSrc;
+
+    private float AudioVolume = 1f;
+
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        AudioSrc = GetComponent<AudioSource>();
+
+ 
+            resolutions = Screen.resolutions;
+            resolutionDropdown.ClearOptions();
+
+            List<string> options = new List<string>();
+
+            int currentResolutionIndex = 0;
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                options.Add(option);
+
+                if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = i;
+                }
+            }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+        }
+
+    void Update()
+    {
+        AudioSrc.volume = AudioVolume;
+    }
+
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetVolume(float vol)
+    {
+        AudioVolume = vol;
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
