c5ce3bf270b081fd69c7b08df07ebe74db4728c4
Bartekp11 <pawlik711@gmail.com>
2020-12-28 19:53:56 +0100

    Enemies and attack
    Animacja oraz mozliwosc atakowania, przeciwnik z animacja smierci

---
+++ Assets/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        animator.SetTrigger("Hit");
+        if(currentHealth <= 0)
+        {
+        Death();
+        }
+    }
+
+    void Death()
+    {
+        animator.SetBool("IsDead", true);
+
+        
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+
+}
