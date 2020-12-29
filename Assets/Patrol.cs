7c3958efa802af7b510d0fc1a7b9a64f24c155e5
Bartekp11 <pawlik711@gmail.com>
2020-12-29 18:42:57 +0100

    Simple enemy AI

---
+++ Assets/Patrol.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+  
+    [SerializeField] float speed = 1f;
+    Rigidbody2D myRigidBody;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(IsFacingRight())
+        {
+            myRigidBody.velocity = new Vector2(speed, 0f);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector2(-speed, 0f);
+        }
+    }
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+    private void OnTriggerExit2D(Collider2D collison)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), transform.localScale.y);
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /* public float speed;
+   private bool movingRight = true;
+   public Transform groundDetection;
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+        if(groundInfo.collider == false)
+        {
+            if(movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }*/
+}
